bc49b34821d010a855e95330b10e1400e07a2dbc
Daniel Friedman <danielnfriedman@gmail.com>
2013-03-02 18:56:31 -0800

    report counts

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,24 @@
+var count = 0;
+
 var App = {
+
+  deviceCounts:{},
 
   init: function() {
     var pusher = new Pusher(App.user.pusherKey);
     var channel = pusher.subscribe(App.user.pusherChannel);
     channel.bind('data', function(device) {
       if (device.D !== 11) return;
-      console.log(device);
+
+      App.deviceCounts[device.DA] = App.deviceCounts[device.DA] || 0;
+      ++App.deviceCounts[device.DA];
     });
+
+    setInterval(function() {
+      console.log(App.deviceCounts);
+      App.deviceCounts = {};
+    },60000);
+
   }
 
 };
