c752e65c0a7557b5524eb114b4b56a11a37008dd
Daniel Friedman <danielnfriedman@gmail.com>
2013-03-02 17:16:45 -0800

    actually add proxy code

--- routes/index.js
+++ routes/index.js
@@ -17,3 +17,15 @@ exports.handleNinjaAuthentication = function(req,res,ninja) {
   req.session.token = ninja.token;
   res.redirect('/');
 };
+
+exports.proxy = function(req,res) {
+
+  var query = req.query;
+  query.access_token = req.session.token;
+  request({
+      url: 'https://api.ninja.is'+req.url,
+      method: req.method,
+      qs: query,
+      json: req.body
+  }).pipe(res);
+};
